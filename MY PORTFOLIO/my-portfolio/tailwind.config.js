6aadafd2a62dd45ec0faf4b7df5e49957cd6586a
Oitanny <mannyk778@gmail.com>
2023-03-31 23:01:22 +0530

    add loader

---
+++ MY PORTFOLIO/my-portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{ts,js,tsx,jsx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        burtons:"burtons",
+      }
+    },
+  },
+  plugins: [],
+}
